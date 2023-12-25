7a6029ae5dd9d1fdd544e1b16feb55a5c4e759b7
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-12-25 01:04:05 -0800

    fix error

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """function that returns Hello HBNB"""
-    return "<p>Hello HBNB!</p>"
+    return "Hello HBNB!"
 
 
 if __name__ == '__main__':

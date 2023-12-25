8edda7e9c0a24d27c262281ff6a9bc76dc12aad3
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-12-25 01:05:29 -0800

    fix error

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -16,13 +16,13 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """function that returns Hello HBNB"""
-    return "<p>Hello HBNB!</p>"
+    return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """function that defines another route"""
-    return "<p>HBNB</p>"
+    return "HBNB"
 
 
 if __name__ == '__main__':

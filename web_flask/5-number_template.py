f64b43b95f5998550c5c427119d258798b4790d4
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-12-26 09:14:15 -0800

    add task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -18,7 +18,7 @@ A script that starts a Flask web application:
     -   Uses the option strict_slashes=False in the route definition
 """
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -53,22 +53,15 @@ def number(n):
     """function that display “n is a number” only if n is an integer"""
     return f'{n} is a number'
 
+
 @app.route("/number_template/<int:n>", strict_slashes=False)
-def number(n):
+def number_template(n):
     """
         function that:
             display a HTML page only if n is an integer:
             H1 tag: “Number: n” inside the tag BODY
     """
-    return f'<!DOCTYPE html>
-    <HTML lang="en">
-        <HEAD>
-            <TITLE>HBNB</TITLE>
-        </HEAD>
-        <BODY>
-            <H1>Number: {n}</H1>
-        </BODY>
-    </HTML>'
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':

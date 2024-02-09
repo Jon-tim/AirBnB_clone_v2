82366d504e50b83b0a6599eafdbef92d746a4cdc
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2024-02-09 06:38:52 -0800

    fix error

--- models/base_model.py
+++ models/base_model.py
@@ -13,13 +13,12 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         if kwargs:
-            self.id = str(uuid.uuid4())
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    else:
+                        setattr(self, key, value)
         storage.new(self)
 
     def __str__(self):
